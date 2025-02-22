6290d9df2fbcbc9bfb45a1cbfc48db9446ea98d1
zen20031566 <tanzenyoung1@gmail.com>
2025-02-22 13:28:08 +0800

    meow
    meow meow

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,18 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private Camera camera1;
+    [SerializeField] private Camera camera2;
+    [SerializeField] private Camera mergedCamera;
+    [SerializeField] private float mergeDistance = 5f; // Distance to merge cameras
+    private const string MAIN_CAMERA = "MainCamera";
+    private const string UNTAGGED = "Untagged";
+
+    private bool isMerged = false;
+
     void Start()
     {
-        
+        mergedCamera.enabled = false; // Start with merged camera disabled
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector2.Distance(player1.position, player2.position);
+
+        if (distance <= mergeDistance && !isMerged)
+        {
+            MergeCameras();
+        }
+        else if (isMerged)
+        {
+            // Check if players are outside merged camera's view
+            if (IsOutsideCameraView(player1) || IsOutsideCameraView(player2))
+            {
+                SplitCameras();
+            }
+        }
+    }
+
+    private void MergeCameras()
+    {
+        isMerged = true;
+
+        camera1.enabled = false;
+        camera2.enabled = false;
+        mergedCamera.enabled = true;
+
+        // Set mergedCamera as the active camera
+        mergedCamera.tag = MAIN_CAMERA;
+        camera1.tag = UNTAGGED;
+        camera2.tag = UNTAGGED;
+
+        // Center merged camera between players (fix Y to 5)
+        mergedCamera.transform.position = new Vector3(
+            (player1.position.x + player2.position.x) / 2, // Center X
+            5, // Keep Y at 5
+            -10f // Keep Z at -10
+        );
+    }
+
+    private void SplitCameras()
+    {
+        isMerged = false;
+
+        camera1.enabled = true;
+        camera2.enabled = true;
+        mergedCamera.enabled = false;
+
+        // Restore original cameras as MainCamera
+        camera1.tag = MAIN_CAMERA;
+        camera2.tag = MAIN_CAMERA;
+        mergedCamera.tag = UNTAGGED;
+    }
+
+    private bool IsOutsideCameraView(Transform player)
+    {
+        Vector3 viewportPos = mergedCamera.WorldToViewportPoint(player.position);
+        return viewportPos.x < 0 || viewportPos.x > 1 || viewportPos.y < 0 || viewportPos.y > 1;
     }
 }
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float moveSpeed = 15f;
+    [SerializeField] private float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private const string GROUND = "Ground";
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement() {
         
+        float moveInput = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            moveInput = -1f; // Move left
+        } 
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            moveInput = 1f; // Move right
+        }
+
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void HandleJump() {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false; 
+        }
+    }
+
+    //Ground Check
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND))
+        {
+            isGrounded = true;
+        }
     }
 }
